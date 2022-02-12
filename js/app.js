716d0d18a78ecfea8f2c81a6a52948486e59b1d4
Nathan Waters <nathanwaters94@gmail.com>
2022-02-11 22:35:53 -0800

    question 5 solved

--- js/app.js
+++ js/app.js
@@ -100,13 +100,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let product = 0;
 
+  for(let i = 0; i < multArr.length; i++) {
+    if(i === 0){
+      product = multArr[0];
+    } else {
+      product = multiply(product, multArr[i])[0];
+    }
+  }
+  return [product, `The numbers ${multArr} have a product of ${product}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
